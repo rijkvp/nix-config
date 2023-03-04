0af502b5c3802357cad7bb27e4886c26b63df36f
rijkvp <rijk@rijkvp.nl>
2023-03-04 10:57:15 +0100

    Stable hyprland

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland/v0.22.0beta";
     agenix.url = "github:ryantm/agenix";
   };
 
