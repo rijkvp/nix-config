445fb76539dc4d2d5f643dace16d55111fdef394
rijkvp <rijk@rijkvp.nl>
2023-04-29 10:28:21 +0200

    Wrap toggle keybind in NeoVim

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -19,8 +19,6 @@
       
       vim.opt.wrap = true
       vim.opt.linebreak = true
-      vim.keymap.set('n', 'j', 'gj')
-      vim.keymap.set('n', 'k', 'gk')
 
       vim.opt.swapfile = false
       vim.opt.backup = false
@@ -36,9 +34,14 @@
       vim.opt.showmode = false
 
       vim.opt.updatetime = 50
+
       -- Remaps
       vim.g.mapleader = " "
-      vim.keymap.set("n", "<leader>o", vim.cmd.Ex)
+      vim.keymap.set('n', '<leader>o', vim.cmd.Ex)
+
+      vim.keymap.set('n', 'j', 'gj')
+      vim.keymap.set('n', 'k', 'gk')
+      vim.keymap.set('n', '<leader>w', ':set wrap!<CR>')
     '';
     plugins = with pkgs.vimPlugins; [
       # Theme
