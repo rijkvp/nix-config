d92e51c327d3551f65b441c9d0e9c9c73b2443aa
Rijk van Putten <rijk@rijkvp.nl>
2024-12-19 19:27:29 +0100

    Update nvim keybinds & try out Zen kernel

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -253,10 +253,10 @@
               vim.keymap.set("n", "gi", require('telescope.builtin').lsp_implementations, opts)
               vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
               vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, opts)
-              vim.keymap.set("n", "<leader>sf", require('telescope.builtin').lsp_workspace_symbols, opts)
+              vim.keymap.set("n", "gs", require('telescope.builtin').lsp_document_symbols, opts)
+              vim.keymap.set("n", "gS", require('telescope.builtin').lsp_workspace_symbols, opts)
 
-              -- Actions
-              vim.keymap.set("n", "<leader>c", vim.lsp.buf.code_action, opts)
+              vim.keymap.set("n", "g.", vim.lsp.buf.code_action, opts)
               vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
               vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
             end,
--- hosts/zeus/default.nix
+++ hosts/zeus/default.nix
@@ -9,6 +9,13 @@
     ../common/podman.nix
   ];
 
+  # Zen kernel with full preemption
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelParams = [
+    "preempt=full"
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+  ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -25,23 +32,25 @@
   };
 
   # Hardware
-  hardware = {
-    enableAllFirmware = true;
-    graphics.enable = true;
-  };
+  hardware.enableAllFirmware = true;
 
   # Bluetooth
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
+  boot.kernelModules = [
+    "btusb"
+    "bluetooth"
+  ];
 
-  # Nvidia
+  # Nvidia graphics
+  hardware.graphics.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
-    open = false;
+    open = true;
     powerManagement.enable = true;
   };
   services.xserver.videoDrivers = [ "nvidia" ];
-  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+  #boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ]; already defined above
   environment.systemPackages = with pkgs; [ egl-wayland ];
 
   # Internal Hard Drive
